d23847af1adc464f79328ef189047ae53931a401
Aldo Verdugo <averdugo@moviit.com>
2016-11-18 16:09:45 -0300

    init

---
+++ resources/views/web/footer.blade.php
@@ -0,0 +1,29 @@
+<section class="mbr-section mbr-section-md-padding mbr-footer footer1" id="index-contacts1-0" style="background-color: rgb(0, 0, 0); padding-top: 30px; padding-bottom: 30px;">
+
+	<div class="container">
+		<div class="row">
+			<div class="mbr-footer-content col-xs-12 col-md-3">
+				<div><img src="assets/images/logo2-128x128-50.png" alt="DrinksCity"></div>
+			</div>
+			<div class="mbr-footer-content col-xs-12 col-md-3">
+				<p><strong>Dirección</strong><br>XXX</p>
+			</div>
+			<div class="mbr-footer-content col-xs-12 col-md-3">
+				<p><strong>Contacto</strong><br>
+				Email: xxx@drinkscity.cl<br>
+				Teléfono:: +569 xxx xxx xxx<br><br></p>
+			</div>
+			<div class="mbr-footer-content col-xs-12 col-md-3">
+				<p><br><br><br></p>
+			</div>
+
+		</div>
+	</div>
+</section>
+
+<footer class="mbr-small-footer mbr-section mbr-section-nopadding" id="index-footer1-0" style="background-color: rgb(0, 0, 0); padding-top: 1.75rem; padding-bottom: 1.75rem;">
+
+    <div class="container">
+        <p class="text-xs-center">Copyright (c) 2016 DrinksCity</p>
+    </div>
+</footer>
---
+++ resources/views/web/nav.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-dropdown bg-color transparent navbar-fixed-top">
+    <div class="container">
+
+        <div class="mbr-table">
+            <div class="mbr-table-cell">
+
+                <div class="navbar-brand">
+                    <span class="navbar-logo"><img src="assets/images/logo-146x128-23.png" alt="Drink City"></span>
+                    <a class="navbar-caption" href="#">DRINKS CITY</a>
+                </div>
+
+            </div>
+            <div class="mbr-table-cell">
+
+                <button class="navbar-toggler pull-xs-right hidden-md-up" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                    <div class="hamburger-icon"></div>
+                </button>
+
+                <ul class="nav-dropdown collapse pull-xs-right nav navbar-nav navbar-toggleable-sm" id="exCollapsingNavbar"><li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="#" aria-expanded="false">QUIENES SOMOS</a><div class="dropdown-menu"><a class="dropdown-item" href="index.html#index-header1-0">¿Qué es DrinksCity?</a><a class="dropdown-item" href="#">Bases Legales</a><a class="dropdown-item" href="#">Tú Marca en DrinksCity</a><a class="dropdown-item" href="#">Más información</a></div></li><li class="nav-item dropdown"><a class="nav-link link dropdown-toggle" data-toggle="dropdown-submenu" href="#" aria-expanded="false">AYUDA</a><div class="dropdown-menu"><a class="dropdown-item" href="#">Video Tutorial</a><a class="dropdown-item" href="#">Escríbenos</a></div></li><li class="nav-item"><a class="nav-link link" href="index.html#index-contacts1-0">CONTACTO</a></li><li class="nav-item nav-btn"><a class="nav-link btn btn-white btn-white-outline" href="#">REGISTRO</a></li></ul>
+                <button hidden="" class="navbar-toggler navbar-close" type="button" data-toggle="collapse" data-target="#exCollapsingNavbar">
+                    <div class="close-icon"></div>
+                </button>
+
+            </div>
+        </div>
+
+    </div>
+</nav>
