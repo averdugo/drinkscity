e991b0d70758af134f2d99a0ffdcd8d5a9e7d5b5
Aldo win10 <aldo.vc.1985@gmail.com>
2017-01-24 06:22:15 -0300

    asdazd

--- public/js/lib/main.js
+++ public/js/lib/main.js
@@ -1,15 +1,16 @@
-google.maps.event.addDomListener(window,'load',()=>{
-	const mapOptions = {
-		zoom:15,
-		center:{
-			lat: 0,
-			lng:0
+google.maps.event.addDomListener(window, 'load', ()=>{
+	const user_location = new UserLocation(()=>{
+		console.log(user_location);
+		const mapOptions = {
+			zoom:15,
+			center:{
+				lat: user_location.latitude,
+				lng:user_location.longitud
+			}
 		}
-	}
-	const mapa_element = document.getElementById('map');
-	const map = new.google.maps.Map(mapa_element, mapOptions);
+		const mapa_element = document.getElementById('map');
+		const map = new google.maps.Map(mapa_element, mapOptions);
+	})
+	
 })
-const user_location = new UserLocation(()=>{
-	console.log(user_location);
 
-})
--- public/js/src/main.js
+++ public/js/src/main.js
@@ -1,16 +1,16 @@
 'use strict';
 
 google.maps.event.addDomListener(window, 'load', function () {
-	var mapOptions = {
-		zoom: 15,
-		center: {
-			lat: 0,
-			lng: 0
-		}
-	};
-	var mapa_element = document.getElementById('map');
-	//const map = new.google.maps.Map(mapa_element, mapOptions);
-});
-var user_location = new UserLocation(function () {
-	console.log(user_location);
+	var user_location = new UserLocation(function () {
+		console.log(user_location);
+		var mapOptions = {
+			zoom: 15,
+			center: {
+				lat: user_location.latitude,
+				lng: user_location.longitud
+			}
+		};
+		var mapa_element = document.getElementById('map');
+		var map = new google.maps.Map(mapa_element, mapOptions);
+	});
 });
