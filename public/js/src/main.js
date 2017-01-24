f2c42fafe2a5eca6b365d4ac513f4aa57b98db8e
Aldo Verdugo <averdugo@moviit.com>
2017-01-23 23:07:51 -0300

    log for maps

---
+++ public/js/src/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+google.maps.event.addDomListener(window, 'load', function () {
+	var mapOptions = {
+		zoom: 15,
+		center: {
+			lat: 0,
+			lng: 0
+		}
+	};
+	var mapa_element = document.getElementById('map');
+	//const map = new.google.maps.Map(mapa_element, mapOptions);
+});
+var user_location = new UserLocation(function () {
+	console.log(user_location);
+});
