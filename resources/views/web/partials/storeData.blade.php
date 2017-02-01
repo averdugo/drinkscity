9947ec023ce04452e476c026d4e8fe0661c7e506
Aldo win10 <aldo.vc.1985@gmail.com>
2017-02-01 00:36:12 -0300

    marker 3

--- resources/views/web/partials/storeData.blade.php
+++ resources/views/web/partials/storeData.blade.php
@@ -1,25 +1,15 @@
 <div id="storeData" class="modal fade bs-example-modal-lg" tabindex="-1" role="dialog" aria-hidden="true">
-	<div class="modal-dialog modal-sm">
+	<div class="modal-dialog modal-lg">
 		<div class="modal-content">
-			<div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">×</span></button>
-				<h4 class="modal-title" id="myModalLabel">Tienda <span id="nombreTienda"></span></h4>
-			</div>
-			
-				<div class="modal-body">
-
-					<div id="dataModalStore"></div>
-					<div>
-						<br>
-						<br><br>
-						<button class="btn btn-primary btn-block" style="padding: 10px">
-							AVISOS
-						</button>
-					</div>
-
-
+			<div class="modal-body">
+				<div id="dataModalStore"></div>
+				<div>
+					<br>
+					<button class="btn btn-primary btn-block" style="padding: 10px">
+						AVISOS
+					</button>
 				</div>
-
+			</div>
 		</div>
 	</div>
 </div>
