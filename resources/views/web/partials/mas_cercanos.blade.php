e853a222de929b9f283380b5b63ae9ef852d7927
Aldo win10 <aldo.vc.1985@gmail.com>
2017-02-02 01:40:04 -0300

    bug

--- resources/views/web/partials/mas_cercanos.blade.php
+++ resources/views/web/partials/mas_cercanos.blade.php
@@ -6,12 +6,12 @@
 		@foreach ($promociones as $pros)
 			<div class="col-xs-12 col-sm-6 col-md-3 anuncioBox" style="margin-bottom:20px;">
 				<div class="anuncioImgbox">
-					<img src="/img/avisos/{{$des->imagen}}" alt="" class="avisoImg" />
+					<img src="/img/avisos/{{$pros->imagen}}" alt="" class="avisoImg" />
 				</div>
 				<div class="text-center" style="margin-top: 20px;">
-					<span class="tituloHomeShow">{{$des->titulo}}</span><br>
-					<span class="tiendaHomeShow">{{$des->store_id}}</span><br>
-					<span class="precioHomeShow">{{$des->precio}}</span><br>
+					<span class="tituloHomeShow">{{$pros->titulo}}</span><br>
+					<span class="tiendaHomeShow">{{$pros->store_id}}</span><br>
+					<span class="precioHomeShow">{{$pros->precio}}</span><br>
 					<span class="distanciaHomeShow"></span>
 				</div>
 			</div>
